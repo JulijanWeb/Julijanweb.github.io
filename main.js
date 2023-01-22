17ecca23dbbb2af4efaf2c9b044cee9e1b718c72
Dzonitheslav <93213808+Dzonitheslav@users.noreply.github.com>
2023-01-22 17:39:35 +0100

    Initiral commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const menu_gumb = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mob-nav');
+
+menu_gumb.addEventListener('click', () => {
+    menu_gumb.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+});
