052f2b1e2971ffd6535aaf4feb04d71f1ddf3508
JulijanWeb <orestwebdesign@gmail.com>
2023-11-10 01:28:35 +0100

    graphic design update

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-const menu_gumb = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.mob-nav');
+const menu_gumb = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".mob-nav");
 
-menu_gumb.addEventListener('click', () => {
-    menu_gumb.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
+menu_gumb.addEventListener("click", () => {
+  menu_gumb.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
 });
 
+function openLightbox(imagePath) {
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImage = document.getElementById("lightboxImage");
 
+  lightboxImage.src = imagePath;
+  lightbox.style.display = "flex";
+}
+
+function closeLightbox() {
+  const lightbox = document.getElementById("lightbox");
+  lightbox.style.display = "none";
+}
